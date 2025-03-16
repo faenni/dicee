58ea95bdff11b837ca93a9186fa7e1c30b017465
faenni <64699507+faenni@users.noreply.github.com>
2025-03-16 18:18:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (randomNumber2 < randomNumber1) {
    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
 else if (randomNumber1 < randomNumber2) {
- document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+ document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
  document.querySelector("h1").innerHTML = "Draw!";
